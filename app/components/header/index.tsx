1b017345b5995b59bd946f12cda9397736722568
Hugo Ricardo <ricardodev08@gmail.com>
2024-02-16 15:23:39 -0300

    feat: created header

---
+++ app/components/header/index.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+
+import Image from "next/image"
+import Link from "next/link"
+import { NavItems } from "./nav-items"
+
+const NAV_ITEMS = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Projetos',
+        href: '/projects'
+    }
+]
+
+export const Header = () => {
+    return(
+        <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center" >
+            <div className="container flex items-center justify-between" >
+                <Link href="/">
+                    <Image
+                    width={58}
+                    height={49}
+                    src="/images/logo.svg"
+                    alt="Logo do portifólio"
+                    />
+                </Link>
+
+                <nav className="flex items-center gap-4 sm:gap-10" >
+                    {NAV_ITEMS.map(item => (
+                        <NavItems {...item} key={item.label} />
+                    ))}
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ app/components/tech-badge/index.tsx
@@ -0,0 +1,14 @@
+
+type TechBadgeProps = {
+    name: string
+}
+
+const TechBadge = ({ name }: TechBadgeProps) => {
+    return ( 
+        <span className="text-emerald-400 bg-emerald-900/80 text-sm py-1 px-3 rounded-lg" >
+            {name}
+        </span>
+     );
+}
+ 
+export default TechBadge;
