e8ec6d54d72b75289ea1cf1c4af48e31d0596ca1
Hugo Ricardo <ricardodev08@gmail.com>
2024-02-16 16:31:49 -0300

    feat: hero-section created

---
+++ app/components/pages/home/highlighted-projects/index.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/components/section-title";
+
+const HighLightedProjects = () => {
+    return ( 
+        <section className="container py-16" >
+            <SectionTitle title="Projetos em destaque" subtitle="destaques" />
+        </section>
+     );
+}
+ 
+export default HighLightedProjects;
